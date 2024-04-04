f0952b49bdeabfaba58261363bf8ce5c291fbe58
Brian Doyle <brian.doyle@coinbase.com>
2024-04-04 13:53:41 -0400

    Add logs

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
+  console.log('getResponse');
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
@@ -36,6 +37,12 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
+  console.log('POST');
+  return getResponse(req);
+}
+
+export async function GET(req: NextRequest): Promise<Response> {
+  console.log('GET');
   return getResponse(req);
 }
 
